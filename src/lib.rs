3e4778dffcee2bfe247b22457ba72650f8154190
Jaimin Pandya <Jaimin.p@alufplastics.com>
2019-02-27 13:56:50 -0500

    Refactored to more rusty code

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-  let mut multiples: Vec<u32> = Vec::new();
-  for multiplier in 1..=limit {
-    factors.iter().for_each(|factor| {
-      if factor * multiplier < limit {
-        multiples.push(factor * multiplier);
-      }
-    });
-  }
-  multiples.sort();
-  multiples.dedup();
-  multiples.iter().sum()
+  let multiples = (1..=limit)
+    .into_iter()
+    .map(|x| {
+      let multiples = factors
+        .iter()
+        .map(move |factor| {
+          let multiple = factor * x;
+          if multiple < limit {
+            return multiple;
+          }
+          0
+        })
+        .collect::<Vec<u32>>();
+      multiples
+    })
+    .flatten()
+    .collect::<Vec<u32>>();
+  let deduped_multiples: HashSet<_> = multiples.iter().cloned().collect();
+  deduped_multiples.iter().sum()
 }
